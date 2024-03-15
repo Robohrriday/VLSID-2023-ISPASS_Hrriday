cbb485a261d5a4714401d7b9a82c3b07fd4f9882
Robohrriday <robohrriday.8@gmail.com>
2024-03-15 10:55:53 +0530

    new uart_rx.py

--- uart_rx.py
+++ uart_rx.py
@@ -1,8 +1,9 @@
+# Python
 import serial           # import the module
 import struct
 import time
 
-ComPort = serial.Serial('COM12') # open COM24
+ComPort = serial.Serial('COM12') # open COM12
 ComPort.baudrate = 9600 # set Baud rate to 9600
 ComPort.bytesize = 8    # Number of data bits = 8
 ComPort.parity   = 'N'  # No parity
@@ -13,24 +14,17 @@ ComPort.stopbits = 1    # Number of Stop bits = 1
 
 for i in range(31):
     x=(input(f"Enter number {i+1}: "))
+    ComPort.flushOutput()
+    ComPort.flushInput()
     if x == 'add':
         break
     x = int(x)
     y = x
     if x>127:
         x = x - 256
-    # if x == 'add':
-    #     break
-    ComPort.flushOutput()
-    ComPort.flushInput()
     ot= ComPort.write(struct.pack('b', x))    #for sending data to FPGA
-    # ComPort.flushOutput()
-    # ComPort.flushInput()
-    # y=input("Enter number 2: ")
-    # ot= ComPort.write(struct.pack('b', int(y)))    #for sending data to FPGA
-    # ComPort.flushOutput()
-    # ComPort.flushInput()
-
+ComPort.flushOutput()
+ComPort.flushInput()
 it=(ComPort.read(size=1))                #for receiving data from FPGA
 print(f"Sum = {int.from_bytes(it, byteorder='little')}")
-ComPort.close()         # Close the Com port        
+ComPort.close()         # Close the Com port
